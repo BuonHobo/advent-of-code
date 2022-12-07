9fe1e2151b502207d197117e86a46f7425d2ae15
buonhobo <naturytropik@gmail.com>
2022-12-07 15:35:14 +0100

    added 7

---
+++ 2022/7/Alex/first.py
@@ -0,0 +1,66 @@
+class File:
+    def __init__(self,name:str,size:int,dir) -> None:
+        self.name=name
+        self.size=size
+        self.parent:Dir=dir
+
+    def get_size(self)->int:
+        return self.size
+
+class Dir:
+    def __init__(self,name:str,dir) -> None:
+        self.name=name
+        self.files:dict[str,Dir|File]={}
+        self.parent:Dir=dir
+
+    def addChild(self,child:File):
+        self.files[child.name]=child
+
+    def getChild(self,child):
+
+        return self.files.get(child,None)
+
+    def get_dirs(self):
+        res=[self]
+        for entry in self.files.values():
+            if type(entry)==Dir:
+                res.extend(entry.get_dirs())
+        return res
+
+    def get_size(self):
+        return sum(entry.get_size() for entry in self.files.values())
+
+
+def main(input:str)->int:
+    root=Dir("/",None)
+    current=root
+
+    for string in input.split("$")[1:]:
+        lines=[line.strip() for line in string.splitlines()]
+        splitcom=lines[0].split()
+        output=lines[1:]
+
+        match splitcom[0]:
+            case "cd":
+                match splitcom[1]:
+                    case "/":
+                        current=root
+                    case "..":
+                        current=current.parent
+                    case other:
+                        current=current.getChild(other)
+            case "ls":
+                for line in output:
+                    info,name=line.split()
+                    if info.isdigit():
+                        current.addChild(File(name,int(info),current))
+                    else:
+                        current.addChild(Dir(name,current))
+
+    return sum((lambda x: x if x<=100000 else 0)(entry.get_size()) for entry in root.get_dirs())
+
+if __name__=="__main__":
+    with open("input.txt") as f:
+        input=f.read()
+    
+    print(main(input))
